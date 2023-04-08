c02a8096a5e30e46c9edff26d9760c550cb4afba
maru-CF-SV <maruk_777@zoho.com>
2023-04-08 22:02:34 +0900

    vueの基本を追加

---
+++ サンプルプログラム/41-my-project/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="color-change">{{msg}}</div>
+</template>
+<script>
+/* Option API pattern */
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Vue 3!'
+    }
+  }
+}
+/* Composition API pattern */
+/*
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive({
+      msg: 'Welcome to Vue 3!'
+    });
+
+    return state;
+  }
+}*/
+</script>
+<style>
+.color-change {
+  color: red;
+}
+</style>
---
+++ サンプルプログラム/41-my-project/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const app = createApp(HelloWorld)
+
+app.mount('#app')
